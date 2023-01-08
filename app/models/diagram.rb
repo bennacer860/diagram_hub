6d8c773535d7a102a7f974c45337d6aadf6c6636
Rafik Bennacer <rafikben@amazon.com>
2023-01-08 18:49:31 -0500

    format the source code display in html

--- app/models/diagram.rb
+++ app/models/diagram.rb
@@ -1,2 +1,12 @@
 class Diagram < ApplicationRecord
+
+
+  def format_source_code_to_html
+    formatted = source_code.gsub(/\n/, '<br/>')
+
+    formatted.split("<br/>").map do |line|
+      leading_spaces_size = line[/\A */].size
+      "#{'&nbsp;'*leading_spaces_size}#{line}"
+    end.join('<br/>')
+  end
 end
