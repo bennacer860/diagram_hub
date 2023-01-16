b19b6aadfa313634d864c63668d2d327f8ea82da
Rafik Bennacer <rafikben@amazon.com>
2023-01-16 09:54:08 -0500

    add support for monaco-editor in the edit view

--- app/models/diagram.rb
+++ app/models/diagram.rb
@@ -2,6 +2,7 @@ class Diagram < ApplicationRecord
 
 
   def format_source_code_to_html
+    return if source_code.nil?
     formatted = source_code.gsub(/\n/, '<br/>')
 
     formatted.split("<br/>").map do |line|
